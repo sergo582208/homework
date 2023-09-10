f0e8eddc6418488ebb4e2b59e284e2df9c48f4b5
hp <747685@mail.ru>
2023-09-11 00:40:18 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        class Student {
+            private String name;
+            private int age;
+
+            public Student(String name, int age) {
+                this.name = name;
+                this.age = age;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (!(o instanceof Student)) return false;
+                Student student = (Student) o;
+                return age == student.age &&
+                        Objects.equals(name, student.name);
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(name, age);
+            }
+        }
     }
 }
