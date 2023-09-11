72ca43898c2a2c53fba26a471c1d6631e980f3de
hp <747685@mail.ru>
2023-09-11 21:52:28 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,29 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        class Student {
-            private String name;
-            private int age;
+class Student{
+    private String name;
+    private int age;
 
-            public Student(String name, int age) {
-                this.name = name;
-                this.age = age;
-            }
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-            @Override
-            public boolean equals(Object o) {
-                if (this == o) return true;
-                if (!(o instanceof Student)) return false;
-                Student student = (Student) o;
-                return age == student.age &&
-                        Objects.equals(name, student.name);
-            }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
 
-            @Override
-            public int hashCode() {
-                return Objects.hash(name, age);
-            }
-        }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
+            }
+
+            }
+
